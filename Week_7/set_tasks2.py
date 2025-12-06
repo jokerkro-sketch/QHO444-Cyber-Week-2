511115f5bb7ce1d36984655655694ee7e24d6137
jokerkro-sketch <jokerkro@wp.pl>
2025-12-06 10:20:23 +0000

    Added code to demonstrate a simple set 2.

--- Week_7/set_tasks2.py
+++ Week_7/set_tasks2.py
@@ -1,10 +1,27 @@
-def observed():
-    observations = {"Car", "Sky Scraper", "Sky Scraper", "Bike", "House", "House"}
+def observed_items():
+    observations = []
+
+    for i in range(7):
+        item = input("Please enter an observation:\n")
+        observations.append(item)
+
     return observations
 
-def run_task1():
-    result = observed()
-    print(result)
+def run_task2():
+    print("Counting observations...")
+
+    data = observed_items()
+
+    result_set = set()
+
+    for value in data:
+        count_tuple = (value, data.count(value))
+        result_set.add(count_tuple)
+
+    print()
+    for item, count in result_set:
+        print(f"{item} observed {count} times.")
+
 
 if __name__ == "__main__":
-    run_task1()
+    run_task2()
