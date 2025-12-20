b3ba52f5cc1067d3c88cf2aae581112bdefda91a
jokerkro-sketch <jokerkro@wp.pl>
2025-12-20 11:14:12 +0000

    added code human.py & robot.py

--- Week_9/human.py
+++ Week_9/human.py
@@ -1,28 +1,16 @@
 # human.py
-# Definicja podstawowej klasy Human
 
 class Human:
-    # Atrybut klasowy (stała wspólna dla wszystkich obiektów)
     MAX_ENERGY = 100
 
     def __init__(self):
-        """
-        Konstruktor klasy Human.
-        Ustawia wartości początkowe dla nowego obiektu.
-        """
-        # Atrybuty instancji
         self.name = "Human"
         self.age = 0
         self.energy = Human.MAX_ENERGY
 
     def display(self):
-        """
-        Metoda instancji wyświetlająca nazwę obiektu.
-        """
         print(f"I am {self.name}")
 
-
-# Kod testowy – wykona się tylko, gdy plik uruchomimy bezpośrednio
 if __name__ == "__main__":
     human = Human()
     human.display()
